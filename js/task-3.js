f72803b4c12e791be5cbdd59ffb1f8ed2fc788cd
unknown <baramikovajane@gmail.com>
2025-09-25 14:59:44 +0300

    add hw 8

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", () => {
-  const textValue = inputEl.value.trim();
-
-  outputEl.textContent = textValue === "" ? "Anonymous" : textValue;
-});
